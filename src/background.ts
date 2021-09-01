b0cd2a46f7d5dca2723439d582f7cc68acbcb79f
Hoshi Ran <ureshinostyle350@gmail.com>
2021-09-01 14:05:33 +0900

    [add] つくよみボイス再生関数でぎだ！

--- src/background.ts
+++ src/background.ts
@@ -4,22 +4,6 @@ import { getStartTime, getInterval, requestAlarm } from './lib/api';
 
 dayjs.extend(duration);
 
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.text === 'alarm') {
-//     const milliseconds = request.milliseconds;
-//     console.log(milliseconds);
-
-//     chrome.alarms.create('interval_notification', {
-//       when: Date.now() + milliseconds,
-//     });
-
-//     sendResponse({ text: `alarm was set after ${milliseconds} milliseconds` });
-//   }
-
-//   sendResponse();
-//   return;
-// });
-
 chrome.alarms.onAlarm.addListener(async (alarm) => {
   if (alarm.name === 'interval_notification') {
     const startTime = await getStartTime();
@@ -28,23 +12,86 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
       throw new Error('startTime is undefined!');
     }
 
+    const elapsedMillisecond = dayjs().diff(dayjs(startTime));
+
     chrome.notifications.create({
       type: 'basic',
       iconUrl: 'logo192.png',
-      title: '定期通知 | 力がつくよみちゃん🥺',
+      title: '定期通知 | 力がつくよみちゃん🌙',
       message: `経過時間：${dayjs
-        .duration(dayjs().diff(dayjs(startTime)))
-        .format('HH時間mm分ss秒')}\n頑張ってるね！🍺`,
+        .duration(elapsedMillisecond)
+        .format('HH時間mm分ss秒')}\n頑張ってるね！🎉`,
     });
 
-    // つくよみちゃんボイス再生
-    new Audio('voices/voice_02_a.wav').play();
+    playTsukuyomiVoice(elapsedMillisecond);
+    alert(elapsedMillisecond);
 
     // 定期実行の再設定
     const interval = await getInterval();
+
     if (interval === undefined) {
       throw new Error('startTime is undefined!');
     }
+
     requestAlarm(interval);
   }
 });
+
+function playTsukuyomiVoice(millisecond: number) {
+  const second = millisecond * 1000;
+  console.log(second);
+  let path = '';
+
+  if (second < 10) {
+    path = 'voices/sec/voice_10sec.wav';
+  } else if (second < 20) {
+    path = 'voices/sec/voice_20sec.wav';
+  } else if (second < 30) {
+    path = 'voices/sec/voice_30sec.wav';
+  } else if (second < 40) {
+    path = 'voices/sec/voice_40sec.wav';
+  } else if (second < 50) {
+    path = 'voices/sec/voice_50sec.wav';
+  } else if (second < 1 * 60) {
+    path = 'voices/min/voice_1min.wav';
+  } else if (second < 2 * 60) {
+    path = 'voices/min/voice_2min.wav';
+  } else if (second < 3 * 60) {
+    path = 'voices/min/voice_3min.wav';
+  } else if (second < 4 * 60) {
+    path = 'voices/min/voice_4min.wav';
+  } else if (second < 5 * 60) {
+    path = 'voices/min/voice_5min.wav';
+  } else if (second < 10 * 60) {
+    path = 'voices/min/voice_10min.wav';
+  } else if (second < 1 * 60 * 60) {
+    path = 'voices/hour/voice_1hour.wav';
+  } else if (second < 2 * 60 * 60) {
+    path = 'voices/hour/voice_1hour.wav';
+  } else if (second < 3 * 60 * 60) {
+    path = 'voices/hour/voice_1hour.wav';
+  } else if (second < 4 * 60 * 60) {
+    path = 'voices/hour/voice_1hour.wav';
+  } else if (second < 5 * 60 * 60) {
+    path = 'voices/hour/voice_1hour.wav';
+  } else {
+    const pathes = [
+      'voices/other/voice_02_a.wav',
+      'voices/other/voice_03_a.wav',
+      'voices/other/voice_03_b.wav',
+      'voices/other/voice_04_a.wav',
+      'voices/other/voice_05_a.wav',
+      'voices/other/voice_05_b.wav',
+      'voices/other/voice_06_a.wav',
+      'voices/other/voice_07_a.wav',
+      'voices/other/voice_07_b.wav',
+      'voices/other/voice_08_a.wav',
+      'voices/other/voice_08_b.wav',
+      'voices/other/voice_09_a.wav',
+      'voices/other/voice_14_a.wav',
+    ];
+    path = pathes[Math.floor(Math.random() * pathes.length)];
+  }
+
+  new Audio(path).play();
+}
